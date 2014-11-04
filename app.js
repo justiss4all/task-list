e19148dca4039276ea59e6b2fde43e2f27f55a88
justiss4all <bmjustiss@gmail.com>
2014-11-04 17:25:20 -0500

    Added code to display multiple entries

--- app.js
+++ app.js
@@ -2,15 +2,30 @@
   var app = angular.module('TaskList', []);
 
   app.controller('TaskController', function(){
-    this.entry = task;
+    this.entries = tasks;
   });
 
-  var task = {
-    name: 'First task',
-    description: 'This is our first task for wireframing',
-    creationDate: '11/3/2014',
-    completionDate: null,
-    completed: false
-  }
-
+  var tasks = [
+    {
+      name: 'First task',
+      description: 'This is our first task for wireframing',
+      creationDate: '1132014',
+      completionDate: null,
+      completed: false,
+    },
+    {
+      name: 'Second task',
+      description: 'This is our second task for wireframing',
+      creationDate: '1288323623006',
+      completionDate: null,
+      completed: false,
+    },
+    {
+      name: 'Third task',
+      description: 'This is our third task for wireframing',
+      creationDate: '11/4/2014',
+      completionDate: null,
+      completed: false,
+    }
+  ];
 })();
