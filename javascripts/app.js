e11298a9cf4d6970031ad64a06e7f5cbd9ca43fe
justiss4all <bmjustiss@gmail.com>
2014-11-16 10:24:35 -0500

    Added  to remove expired tasks

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,20 +2,25 @@
   var app = angular.module('TaskList', ['firebase']);
 
   // List Controller
-  app.controller('ListController',['$scope', 'TasksService', function ($scope, TasksService) {
+  app.controller('ListController',['$scope', '$interval', 'TasksService', function ($scope, $interval, TasksService) {
       $scope.newTask = {
         name: '',
         description: '',
         creationDate: '',
+        expirationDate: '',
         completed: false,
+        expired: false,
         };
 
       $scope.tasks = TasksService.getTasks();
 
+      $scope.filterTask = function() {
+        TasksService.filterTasks();
+      };
 
       $scope.addTask = function() {
         TasksService.addTask(angular.copy($scope.newTask));
-        $scope.newTask = {name: '', description: '', creationDate: '', completed: false};
+        $scope.newTask = {name: '', description: '', creationDate: '', expirationDate: '', completed: false, expired: false};
       };
 
       $scope.updateTask = function(task) {
@@ -26,7 +31,21 @@
         TasksService.removeTask(task);
       }
 
+      // Cycle through tasks to check expiration date
+      $interval(function(){
+          $scope.filterTask();
+      }, 9000);
+
+
   }]);
+
+  // Filter for determining days left
+  app.filter('countdown', function() {
+    return function(expirationDate) {
+
+    };
+  });
+
 
   // Service for seperating task concerns
   app.service('TasksService', ['$firebase', function($firebase) {
@@ -38,14 +57,27 @@
       // download the data to a local array
       var entries = sync.$asArray();
 
+      // filters if past expiration date
+      this.filterTasks = function(){
+        var now = moment().format('MM/DD/YYYY');
+        var entryLength = entries.length;
+        for(var i = 0; i < entryLength; i++){
+            if(entries[i].expirationDate < now && !entries[i].expired) {
+              console.log('Task ' + entries[i].name + ' has been removed');
+              entries[i].expired = true;
+              this.updateTask(entries[i]);
+          }
+        }
+      };
+
       this.getTasks = function(){
         return entries;
       };
 
       this.addTask = function(task){
         // Timestamp creationDate and format with Moment.js
-        var ts = task.creationDate
-        task.creationDate = moment(ts).format("MM/DD/YYYY");
+        task.creationDate = moment().format("MM/DD/YYYY");
+        task.expirationDate = moment().add(7, 'days').format("MM/DD/YYYY");
         entries.$add(task);
       };
 
@@ -56,19 +88,10 @@
       this.removeTask = function(task){
         entries.$remove(task);
       };
-
-
   }]);
-
-  app.directive('taskDetails', function(){
-    return {
-      restrict: 'E',
-      templateUrl: '/templates/task-details.html',
-      controller: function(){
-        // this controller will manage individual task f(x)'s
-      },
-      controllerAs: 'taskCtrl',
-    };
-  });
-
 })();
+
+/* Refactoring ToDo:
+-- remove removeTask if not needed
+
+*/
